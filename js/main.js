622f1bf5e8fd678a902641e10457cba72fdb8ec0
Cristian <crai.crai2025@gmail.com>
2026-03-09 10:37:45 -0500

    Initialize Phaser game with configuration

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("archivo principal del juego");
+const config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 700,
+    parent: 'game',
+    scene: [MapScene]
+};
+
+const game = new Phaser.Game(config);
