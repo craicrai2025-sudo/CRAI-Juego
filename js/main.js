adef6e6acf009b8c29a393920bea10050b3774d9
Cristian <crai.crai2025@gmail.com>
2026-03-09 11:32:21 -0500

    Fix formatting in main.js configuration

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const config = {
-type: Phaser.AUTO,
-width: 1200,
-height: 700,
-physics:{
-default:"arcade"
-},
-scene:[MapScene]
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 700,
+
+    physics:{
+        default:"arcade"
+    },
+
+    scene:[MapScene]
 };
 
 const game = new Phaser.Game(config);
