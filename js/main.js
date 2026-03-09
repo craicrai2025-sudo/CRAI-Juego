e26758b05250295c564a943bbb4918ac9cfae2cc
Cristian <crai.crai2025@gmail.com>
2026-03-09 11:27:38 -0500

    Add physics configuration to game initialization
    Added physics configuration to the game setup.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const config = {
-    type: Phaser.AUTO,
-    width: 1200,
-    height: 700,
-    parent: "game",
-    scene: [MapScene]
+type: Phaser.AUTO,
+width: 1200,
+height: 700,
+physics:{
+default:"arcade"
+},
+scene:[MapScene]
 };
 
 const game = new Phaser.Game(config);
