e46674997877356cd2c161f311300c3541536c2c
Cristian <crai.crai2025@gmail.com>
2026-03-09 10:38:07 -0500

    Add MapScene class with avatar movement functionality
    Implement MapScene class with preload and create methods for loading images and handling avatar movement.

--- scenes/MapScene.js
+++ scenes/MapScene.js
@@ -1,1 +1,37 @@
-console.log("escena del mapa");
+class MapScene extends Phaser.Scene {
+
+    constructor(){
+        super("MapScene");
+    }
+
+    preload(){
+
+        // imágenes
+        this.load.image("mapa","assets/mapa.png");
+        this.load.image("avatar","assets/avatar.png");
+
+    }
+
+    create(){
+
+        // fondo del mapa
+        this.add.image(600,350,"mapa");
+
+        // avatar
+        this.avatar = this.physics.add.sprite(600,500,"avatar");
+
+        // click para mover avatar
+        this.input.on("pointerdown",(pointer)=>{
+
+            this.tweens.add({
+                targets:this.avatar,
+                x:pointer.x,
+                y:pointer.y,
+                duration:800
+            });
+
+        });
+
+    }
+
+}
