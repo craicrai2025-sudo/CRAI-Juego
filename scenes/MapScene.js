86facd2fe83e1be1b28354429f814f18693b260e
Cristian <crai.crai2025@gmail.com>
2026-03-10 09:28:19 -0500

    Implement avatar movement with keyboard and mouse

--- scenes/MapScene.js
+++ scenes/MapScene.js
@@ -13,29 +13,86 @@ this.load.image("avatar","assets/avatar.png");
 
 create(){
 
+// MAPA
 this.add.image(600,350,"mapa");
 
+// AVATAR
 this.avatar = this.physics.add.sprite(600,350,"avatar");
 this.avatar.setScale(0.6);
 
+// CONTROLES DE TECLADO
 this.cursors = this.input.keyboard.createCursorKeys();
+
+// DESTINO DEL CLIC
+this.targetX = null;
+this.targetY = null;
+
+// DETECTAR CLIC DEL MOUSE
+this.input.on("pointerdown",(pointer)=>{
+
+this.targetX = pointer.x;
+this.targetY = pointer.y;
+
+});
 
 }
 
 update(){
 
+let speed = 3;
+
+// MOVIMIENTO CON FLECHAS
+
 if(this.cursors.left.isDown){
-this.avatar.x -= 3;
+
+this.avatar.x -= speed;
+this.targetX = null;
+
 }
+
 else if(this.cursors.right.isDown){
-this.avatar.x += 3;
+
+this.avatar.x += speed;
+this.targetX = null;
+
 }
 
 if(this.cursors.up.isDown){
-this.avatar.y -= 3;
+
+this.avatar.y -= speed;
+this.targetX = null;
+
 }
+
 else if(this.cursors.down.isDown){
-this.avatar.y += 3;
+
+this.avatar.y += speed;
+this.targetX = null;
+
+}
+
+
+// MOVIMIENTO CON CLIC
+
+if(this.targetX !== null){
+
+let dx = this.targetX - this.avatar.x;
+let dy = this.targetY - this.avatar.y;
+
+let distance = Math.sqrt(dx*dx + dy*dy);
+
+if(distance > 5){
+
+this.avatar.x += dx * 0.02;
+this.avatar.y += dy * 0.02;
+
+}else{
+
+this.targetX = null;
+this.targetY = null;
+
+}
+
 }
 
 }
