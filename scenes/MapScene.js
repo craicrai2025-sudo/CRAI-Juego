486fafad7bdae5d268b936fa79650014bea8b778
Cristian <crai.crai2025@gmail.com>
2026-03-10 11:20:41 -0500

    Swap floorMinY and floorMaxY values

--- scenes/MapScene.js
+++ scenes/MapScene.js
@@ -32,8 +32,8 @@ this.targetY = null;
 this.floorMinX = 50;
 this.floorMaxX = 1150;
 
-this.floorMinY = 500;
-this.floorMaxY = 200;
+this.floorMinY = 200;
+this.floorMaxY = 500;
 
 // EVENTO DE CLIC
 
