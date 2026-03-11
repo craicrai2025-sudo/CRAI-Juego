85473ef5515577b864ffe4f6b152416a0a0ea19c
Cristian <crai.crai2025@gmail.com>
2026-03-11 15:08:28 -0500

    Update inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -1,1 +1,42 @@
+class Inventory {
 
+constructor(scene){
+
+this.scene = scene;
+this.items = [];
+
+this.text = scene.add.text(
+20,
+20,
+"Inventario: vacío",
+{
+font: "20px Arial",
+fill: "#ffffff",
+backgroundColor: "#000000"
+}
+).setScrollFactor(0);
+
+}
+
+addItem(item){
+
+this.items.push(item);
+this.updateUI();
+
+}
+
+updateUI(){
+
+if(this.items.length === 0){
+
+this.text.setText("Inventario: vacío");
+
+}else{
+
+this.text.setText("Inventario: " + this.items.join(", "));
+
+}
+
+}
+
+}
