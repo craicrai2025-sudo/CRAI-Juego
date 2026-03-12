6903d7fef46f2992b325c746e29eb933ccaeba50
Cristian <crai.crai2025@gmail.com>
2026-03-12 10:35:38 -0500

    Update inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -4,38 +4,66 @@ constructor(scene){
 
 this.scene = scene;
 this.items = [];
+this.icons = [];
 
-this.text = scene.add.text(
-20,
-20,
-"Inventario: vacío",
-{
-font: "20px Arial",
-fill: "#ffffff",
-backgroundColor: "#000000"
+// fondo inventario
+this.background = scene.add.rectangle(
+1000,
+80,
+340,
+120,
+0x000000,
+0.6
+).setDepth(200);
+
+// slots
+this.slots = [];
+
+for(let i=0;i<5;i++){
+
+let slot = scene.add.rectangle(
+880 + (i*65),
+80,
+55,
+55,
+0xffffff,
+0.2
+)
+.setStrokeStyle(2,0xffffff)
+.setDepth(201);
+
+this.slots.push(slot);
+
 }
-).setScrollFactor(0);
 
 }
 
-addItem(item){
+// añadir item
+addItem(texture){
 
-this.items.push(item);
-this.updateUI();
+let index = this.items.length;
 
-}
+this.items.push(texture);
 
-updateUI(){
+let icon = this.scene.add.image(
+880 + (index*65),
+80,
+texture
+)
+.setScale(0.08)
+.setDepth(202)
+.setInteractive({ draggable:true });
 
-if(this.items.length === 0){
+this.scene.input.setDraggable(icon);
 
-this.text.setText("Inventario: vacío");
+this.scene.input.on("drag",(pointer,gameObject,dragX,dragY)=>{
 
-}else{
+gameObject.x = dragX;
+gameObject.y = dragY;
 
-this.text.setText("Inventario: " + this.items.join(", "));
+});
 
-}
+this.icons.push(icon);
 
 }
 
