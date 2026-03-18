4078452942437166a4439733727aea8f1886490b
Cristian <crai.crai2025@gmail.com>
2026-03-18 14:05:40 -0500

    Update MapScene.js

--- scenes/MapScene.js
+++ scenes/MapScene.js
@@ -5,8 +5,7 @@ super("MapScene")
 }
 
 preload(){
-  
-this.load.image("domicilios","assets/centro_domicilios.png")
+
 this.load.image("mapa","assets/mapa.png")
 this.load.image("avatar","assets/avatar.png")
 this.load.image("flecha","assets/flecha.png")
@@ -16,48 +15,25 @@ this.load.image("libro","assets/libro.png")
 
 create(){
 
-
-//Centro de domicilios
-this.add.image(600,350,"domicilios")
-
-// MAPA
-this.mapa = this.add.image(600,350,"mapa")
+// FONDO
+this.add.image(600,350,"mapa")
 
 // INVENTARIO
 this.inventory = new Inventory(this)
 
 // AVATAR
 this.avatar = this.physics.add.sprite(600,550,"avatar")
-this.avatar.setScale(0.6)
+.setScale(0.6)
 
 // CONTROLES
 this.cursors = this.input.keyboard.createCursorKeys()
 this.keyE = this.input.keyboard.addKey("E")
 
 // LIMITES
-this.floorMinX = 50
-this.floorMaxX = 1150
-this.floorMinY = 420
-this.floorMaxY = 690
-
-// ICONO INTERACCION
-this.interactIcon = this.add.text(
-0,
-0,
-"E",
-{
-font:"28px Arial",
-fill:"#ffffff"
-})
-.setPadding(6)
-.setBackgroundColor("#000000")
-.setDepth(300)
-.setVisible(false)
-
-// PUERTA
-this.flecha = this.add.image(420,450,"flecha")
-.setScale(0.15)
-.setAngle(-90)
+this.minX = 50
+this.maxX = 1150
+this.minY = 420
+this.maxY = 690
 
 // LIBRO
 this.libro = this.add.image(1000,520,"libro")
@@ -66,20 +42,29 @@ this.libro = this.add.image(1000,520,"libro")
 
 this.libroRecogido = false
 
-// CLIC LIBRO
+// FLECHA
+this.flecha = this.add.image(420,450,"flecha")
+.setScale(0.15)
+.setAngle(-90)
+.setInteractive()
+
+// ICONO E
+this.iconE = this.add.text(0,0,"E",{
+font:"24px Arial",
+fill:"#ffffff"
+})
+.setBackgroundColor("#000")
+.setPadding(5)
+.setVisible(false)
+
+// CLICK LIBRO
 this.libro.on("pointerdown",()=>{
-
-this.tryCollectBook()
-
+this.recogerLibro()
 })
 
-// CLIC PUERTA
-this.flecha.setInteractive()
-
+// CLICK PUERTA
 this.flecha.on("pointerdown",()=>{
-
 this.scene.start("DomiciliosScene")
-
 })
 
 }
@@ -89,68 +74,49 @@ update(){
 let speed = 2.5
 
 // MOVIMIENTO
-
 if(this.cursors.left.isDown) this.avatar.x -= speed
-else if(this.cursors.right.isDown) this.avatar.x += speed
-
+if(this.cursors.right.isDown) this.avatar.x += speed
 if(this.cursors.up.isDown) this.avatar.y -= speed
-else if(this.cursors.down.isDown) this.avatar.y += speed
+if(this.cursors.down.isDown) this.avatar.y += speed
 
 // LIMITES
+this.avatar.x = Phaser.Math.Clamp(this.avatar.x,this.minX,this.maxX)
+this.avatar.y = Phaser.Math.Clamp(this.avatar.y,this.minY,this.maxY)
 
-this.avatar.x = Phaser.Math.Clamp(this.avatar.x,this.floorMinX,this.floorMaxX)
-this.avatar.y = Phaser.Math.Clamp(this.avatar.y,this.floorMinY,this.floorMaxY)
-
-// PROFUNDIDAD RPG
-
+// PROFUNDIDAD
 this.avatar.setDepth(this.avatar.y)
 
-// DISTANCIA LIBRO
-
+// DETECCION LIBRO
 if(!this.libroRecogido){
 
 let dist = Phaser.Math.Distance.Between(
-this.avatar.x,
-this.avatar.y,
-this.libro.x,
-this.libro.y
+this.avatar.x,this.avatar.y,
+this.libro.x,this.libro.y
 )
 
 if(dist < 80){
 
-this.interactIcon.setPosition(this.libro.x,this.libro.y-40)
-this.interactIcon.setVisible(true)
+this.iconE.setPosition(this.libro.x,this.libro.y-40)
+this.iconE.setVisible(true)
 
 if(Phaser.Input.Keyboard.JustDown(this.keyE)){
-
-this.tryCollectBook()
-
+this.recogerLibro()
 }
 
 }else{
-
-this.interactIcon.setVisible(false)
-
+this.iconE.setVisible(false)
 }
 
 }
 
 }
 
-// RECOGER LIBRO
-
-tryCollectBook(){
+recogerLibro(){
 
 if(this.libroRecogido) return
 
 this.inventory.addItem("libro")
-
 this.libro.destroy()
-
 this.libroRecogido = true
-
-this.interactIcon.setVisible(false)
-
-}
 
 }
