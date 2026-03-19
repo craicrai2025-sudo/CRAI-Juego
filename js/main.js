75665b5fdcd84916704b53964e800c1bb9b86745
Cristian <crai.crai2025@gmail.com>
2026-03-19 15:09:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,24 @@ const config = {
 type: Phaser.AUTO,
 width: 1200,
 height: 700,
+
+physics: {   // 🔥 ESTO FALTABA
+default: "arcade",
+arcade: {
+debug: false
+}
+},
+
 scene: [MapScene, DomiciliosScene, UIScene]
 }
 
 const game = new Phaser.Game(config)
 
-// 🔥 ESTADO GLOBAL
+// ESTADO GLOBAL
 game.globalState = {
 mapa: { libroRecogido: false },
 domicilios: { libroRecogido: false }
 }
 
-// 🔥 INICIAR UI
+// INICIAR UI
 game.scene.start("UIScene")
