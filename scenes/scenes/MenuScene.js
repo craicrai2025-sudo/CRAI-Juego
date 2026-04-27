2915cadd6a613d4b302c0adcf8f737e533bbf423
Cristian <crai.crai2025@gmail.com>
2026-04-27 08:29:29 -0500

    Refactor MenuScene to improve button functionality

--- scenes/scenes/MenuScene.js
+++ scenes/scenes/MenuScene.js
@@ -4,77 +4,39 @@ constructor(){
 super("MenuScene")
 }
 
-preload(){
-this.load.image("mapa","assets/mapa.png")
-this.load.image("npc","assets/avatar.png")
-}
-
 create(){
 
-this.add.image(600,350,"mapa").setDisplaySize(1200,700)
-this.add.rectangle(600,350,1200,700,0x000000,0.5)
+this.add.rectangle(600,350,1200,700,0x000000)
 
-this.add.text(400,120,"CRAI AVENTURA",{font:"48px Arial",fill:"#fff"})
+// TITULO
+this.add.text(600,200,"Juego CRAI",{
+font:"48px Arial",
+fill:"#ffffff"
+}).setOrigin(0.5)
 
-// BOTONES
-let iniciar = this.add.text(500,300,"INICIAR JUEGO",{
+// BOTON INICIAR
+let startBtn = this.add.text(600,350,"Iniciar Juego",{
 font:"32px Arial",
-backgroundColor:"#00aa00",
-padding:15
-}).setInteractive()
-
-let config = this.add.text(500,380,"CONFIGURACIÓN",{
-font:"28px Arial",
-backgroundColor:"#555",
-padding:15
-}).setInteractive()
-
-[iniciar, config].forEach(btn=>{
-btn.on("pointerover",()=>btn.setScale(1.1))
-btn.on("pointerout",()=>btn.setScale(1))
+backgroundColor:"#ffffff",
+color:"#000",
+padding:10
 })
+.setOrigin(0.5)
+.setInteractive()
 
-iniciar.on("pointerdown",()=>{
-this.startDialog()
-})
+startBtn.on("pointerdown",()=>{
 
-}
-
-// DIÁLOGO
-startDialog(){
-
-this.children.removeAll()
-
-this.add.image(250,450,"npc").setScale(0.4)
-
-this.box = this.add.rectangle(600,600,1000,150,0x000000,0.8)
-
-this.text = this.add.text(150,550,"Bienvenido al CRAI...",{
-font:"22px Arial",
-fill:"#fff",
-wordWrap:{width:900}
-})
-
-this.dialogs = [
-"Bienvenido al CRAI.",
-"Aquí podrás explorar servicios.",
-"Aprenderás a solicitar libros.",
-"¡Buena suerte!"
-]
-
-this.index = 0
-
-this.input.on("pointerdown",()=>{
-
-this.index++
-
-if(this.index < this.dialogs.length){
-this.text.setText(this.dialogs[this.index])
-}else{
+// iniciar UI + juego
 this.scene.start("MapScene")
-}
+this.scene.launch("UIScene")
 
 })
+
+// BOTON CONFIG (placeholder)
+this.add.text(600,420,"Configuración",{
+font:"28px Arial",
+fill:"#aaa"
+}).setOrigin(0.5)
 
 }
 
