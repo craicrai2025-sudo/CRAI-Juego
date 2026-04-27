2310f278ba6550d8d119b7dbaf21bdd5444d8c14
Cristian <crai.crai2025@gmail.com>
2026-04-27 09:50:32 -0500

    Update UIScene.js

--- scenes/UIScene.js
+++ scenes/UIScene.js
@@ -6,15 +6,11 @@ super("UIScene")
 
 preload(){
 this.load.image("maleta","assets/maleta.png")
-this.load.image("moneda","assets/moneda.png")
-this.load.image("libro","assets/libro.png")
 }
 
 create(){
 
 this.inventory = new Inventory(this)
-
-// GLOBAL
 this.game.inventory = this.inventory
 
 }
