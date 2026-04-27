6c1405c0c96733c1af3eb3dea46adedc2afaaa84
Cristian <crai.crai2025@gmail.com>
2026-04-27 09:43:09 -0500

    Update UIScene.js

--- scenes/UIScene.js
+++ scenes/UIScene.js
@@ -6,20 +6,16 @@ super("UIScene")
 
 preload(){
 this.load.image("maleta","assets/maleta.png")
+this.load.image("moneda","assets/moneda.png")
+this.load.image("libro","assets/libro.png")
 }
 
 create(){
 
 this.inventory = new Inventory(this)
+
+// GLOBAL
 this.game.inventory = this.inventory
 
-// RESTAURAR INVENTARIO
-let saved = this.game.globalState.inventory
-
-saved.forEach(item=>{
-this.inventory.addItem(item)
-})
-
 }
-
 }
