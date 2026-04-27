863725aa184fa5271265c502ff029deebcda99ab
Cristian <crai.crai2025@gmail.com>
2026-04-27 08:28:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ height: 700,
 
 physics: {
 default: "arcade",
-arcade: {
-debug: false
-}
+arcade: { debug: false }
 },
 
 scene: [MenuScene, MapScene, DomiciliosScene, UIScene]
@@ -18,10 +16,5 @@ const game = new Phaser.Game(config)
 // ESTADO GLOBAL
 game.globalState = {
 mapa: { libroRecogido: false },
-domicilios: { libroRecogido: false },
-inventory: []
+domicilios: { libroRecogido: false }
 }
-
-// INICIAR
-game.scene.start("MenuScene")
-game.scene.start("UIScene")
