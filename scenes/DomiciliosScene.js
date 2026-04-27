8bd894f668fbe73a7fa50faf5b9c9dd1781f1fbc
Cristian <crai.crai2025@gmail.com>
2026-04-27 08:23:24 -0500

    Adjust avatar scale and update libro handling

--- scenes/DomiciliosScene.js
+++ scenes/DomiciliosScene.js
@@ -5,11 +5,9 @@ super("DomiciliosScene")
 }
 
 preload(){
-
 this.load.image("domicilios","assets/domicilios.png")
 this.load.image("avatar","assets/avatar.png")
 this.load.image("libro","assets/libro.png")
-
 }
 
 create(){
@@ -17,17 +15,14 @@ create(){
 this.add.image(600,350,"domicilios").setDisplaySize(1200,700)
 
 // AVATAR
-this.avatar = this.physics.add.image(200,300,"avatar").setScale(0.2)
+this.avatar = this.physics.add.image(200,300,"avatar").setScale(0.6)
 
 // LIBRO
+if(!this.game.globalState.domicilios.libroRecogido){
 this.libro = this.physics.add.image(800,500,"libro").setScale(0.08)
-
-// ESTADO
-if(this.game.globalState.domicilios.libroRecogido){
-this.libro.destroy()
 }
 
-// INPUT
+// CONTROLES
 this.cursors = this.input.keyboard.createCursorKeys()
 this.keyE = this.input.keyboard.addKey("E")
 
@@ -36,15 +31,12 @@ this.iconE = this.add.text(0,0,"E",{
 font:"20px Arial",
 fill:"#fff",
 backgroundColor:"#000"
-})
-.setPadding(4)
-.setVisible(false)
+}).setPadding(4).setVisible(false)
 
 }
 
 update(){
 
-// MOVIMIENTO
 if(this.cursors.left.isDown) this.avatar.x -= 3
 if(this.cursors.right.isDown) this.avatar.x += 3
 if(this.cursors.up.isDown) this.avatar.y -= 3
@@ -59,18 +51,15 @@ this.libro.x,
 this.libro.y
 )
 
-// POSICION E
 this.iconE.setPosition(this.avatar.x-10,this.avatar.y-60)
-
-// MOSTRAR
 this.iconE.setVisible(dist < 80)
 
-// RECOGER
 if(dist < 80 && Phaser.Input.Keyboard.JustDown(this.keyE)){
 
 this.game.inventory.addItem("libro")
 
 this.libro.destroy()
+this.libro = null
 this.game.globalState.domicilios.libroRecogido = true
 
 }
