0ca42998bd47f4cad6c63edd98e8c455a6a92dfd
Cristian <crai.crai2025@gmail.com>
2026-04-28 07:03:41 -0500

    Update DomiciliosScene.js

--- scenes/DomiciliosScene.js
+++ scenes/DomiciliosScene.js
@@ -5,9 +5,12 @@ super("DomiciliosScene")
 }
 
 preload(){
-this.load.image("domicilios","assets/centro domicilios.png")
+
+this.load.image("domicilios","assets/centro_domicilios.png") // ✅ CORREGIDO
 this.load.image("avatar","assets/avatar.png")
 this.load.image("libro","assets/libro.png")
+this.load.image("computador","assets/computador.png")
+
 }
 
 create(){
@@ -16,6 +19,9 @@ this.add.image(600,350,"domicilios").setDisplaySize(1200,700)
 
 // AVATAR
 this.avatar = this.physics.add.image(300,400,"avatar").setScale(0.6)
+
+// COMPUTADOR
+this.computador = this.physics.add.image(200,350,"computador").setScale(0.2)
 
 // LIBRO
 this.libro = this.physics.add.image(900,500,"libro").setScale(0.08)
@@ -27,8 +33,6 @@ this.libro.destroy()
 // INPUT
 this.cursors = this.input.keyboard.createCursorKeys()
 this.keyE = this.input.keyboard.addKey("E")
-
-// VOLVER
 this.keyBack = this.input.keyboard.addKey("ESC")
 
 // ICONO E
@@ -39,6 +43,8 @@ backgroundColor:"#000"
 })
 .setPadding(4)
 .setVisible(false)
+
+this.currentTarget = null
 
 }
 
@@ -54,24 +60,45 @@ if(this.cursors.right.isDown) this.avatar.x += 3
 if(this.cursors.up.isDown) this.avatar.y -= 3
 if(this.cursors.down.isDown) this.avatar.y += 3
 
-if(!this.libro) return
-
-let dist = Phaser.Math.Distance.Between(
-this.avatar.x,
-this.avatar.y,
-this.libro.x,
-this.libro.y
-)
-
 this.iconE.setPosition(this.avatar.x-10,this.avatar.y-60)
-this.iconE.setVisible(dist < 80)
 
-if(dist < 80 && Phaser.Input.Keyboard.JustDown(this.keyE)){
+// DISTANCIAS
+let nearLibro = this.libro && Phaser.Math.Distance.Between(
+this.avatar.x,this.avatar.y,this.libro.x,this.libro.y
+) < 80
 
+let nearPC = Phaser.Math.Distance.Between(
+this.avatar.x,this.avatar.y,this.computador.x,this.computador.y
+) < 80
+
+// PRIORIDAD
+if(nearPC){
+this.currentTarget = "pc"
+this.iconE.setVisible(true)
+}
+else if(nearLibro){
+this.currentTarget = "libro"
+this.iconE.setVisible(true)
+}
+else{
+this.currentTarget = null
+this.iconE.setVisible(false)
+}
+
+// INTERACCIÓN
+if(this.currentTarget && Phaser.Input.Keyboard.JustDown(this.keyE)){
+
+if(this.currentTarget === "libro"){
 this.game.inventory.addItem("libro")
-
 this.libro.destroy()
 this.game.globalState.domicilios.libroRecogido = true
+}
+
+if(this.currentTarget === "pc"){
+this.scene.launch("ComputerUI")
+}
+
+}
 
 }
 
